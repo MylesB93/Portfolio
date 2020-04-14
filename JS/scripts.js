1377b4c100dc029020fad7fcfa3a62c4c70c4cd2
Myles Broomes <myles.broomes@etchuk.com>
2020-04-14 12:19:22 +0100

    Display github projects in cards

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,6 +3,12 @@ $.ajax({
 }).done(function(data) { 
     for (var i = 0; i < data.length; i++) {
         // console.log(data[i].name);
-        $('.js-repos').append(`<a href="${data[i].html_url}" target="_blank">${data[i].name}</a><br/>`);
+        $('.js-repos').append(`<div class="card col">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${data[i].name}</h5>
+                                        <p class="card-text">${data[i].description}</p>
+                                        <a href="${data[i].html_url}" target="_blank">View repository</a>
+                                    </div>
+                                </div>`);
     }
 })
